d513de38cfeef44fccde5aeec4a100c861e1cdfa
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-13 09:03:12 +0300

    .

--- src/main/java/com/tugalsan/api/sql/exists/server/TS_SQLExists.java
+++ src/main/java/com/tugalsan/api/sql/exists/server/TS_SQLExists.java
@@ -6,40 +6,42 @@ import com.tugalsan.api.sql.conn.server.TS_SQLConnColUtils;
 import com.tugalsan.api.sql.where.server.TS_SQLWhereConditions;
 import com.tugalsan.api.sql.where.server.TS_SQLWhereGroups;
 import com.tugalsan.api.sql.where.server.TS_SQLWhereUtils;
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
 
-public class TS_SQLExists { 
+public class TS_SQLExists {
 
     public TS_SQLExists(TS_SQLConnAnchor anchor, CharSequence tableName) {
         executor = new TS_SQLExistsExecutor(anchor, tableName);
     }
     private final TS_SQLExistsExecutor executor;
 
-    public boolean whereGroupAnd(TGS_RunnableType1<TS_SQLWhereGroups> groups) {
+    public TGS_UnionExcuse<Boolean> whereGroupAnd(TGS_RunnableType1<TS_SQLWhereGroups> groups) {
         executor.where = TS_SQLWhereUtils.where();
         executor.where.groupsAnd(groups);
         return executor.run();
     }
 
-    public boolean whereGroupOr(TGS_RunnableType1<TS_SQLWhereGroups> groups) {
+    public TGS_UnionExcuse<Boolean> whereGroupOr(TGS_RunnableType1<TS_SQLWhereGroups> groups) {
         executor.where = TS_SQLWhereUtils.where();
         executor.where.groupsOr(groups);
         return executor.run();
     }
 
-    public boolean whereConditionAnd(TGS_RunnableType1<TS_SQLWhereConditions> conditions) {
+    public TGS_UnionExcuse<Boolean> whereConditionAnd(TGS_RunnableType1<TS_SQLWhereConditions> conditions) {
         return whereGroupAnd(where -> where.conditionsAnd(conditions));
     }
 
-    public boolean whereConditionOr(TGS_RunnableType1<TS_SQLWhereConditions> conditions) {
+    public TGS_UnionExcuse<Boolean> whereConditionOr(TGS_RunnableType1<TS_SQLWhereConditions> conditions) {
         return whereGroupOr(where -> where.conditionsOr(conditions));
     }
 
-    public boolean whereFirstColumnAsId(long id) {
+    public TGS_UnionExcuse<Boolean> whereFirstColumnAsId(long id) {
+        var u_names = TS_SQLConnColUtils.names(executor.anchor, executor.tableName);
+        if (u_names.isExcuse()){
+            return u_names.toExcuse();
+        }
         return whereConditionAnd(conditions -> {
-            conditions.lngEq(
-                    TS_SQLConnColUtils.names(executor.anchor, executor.tableName).get(0),
-                    id
-            );
+            conditions.lngEq(u_names.value().get(0), id);
         });
     }
 }
--- src/main/java/com/tugalsan/api/sql/exists/server/TS_SQLExistsExecutor.java
+++ src/main/java/com/tugalsan/api/sql/exists/server/TS_SQLExistsExecutor.java
@@ -1,15 +1,16 @@
 package com.tugalsan.api.sql.exists.server;
 
-import com.tugalsan.api.tuple.client.TGS_Tuple1;
 import com.tugalsan.api.sql.conn.server.TS_SQLConnAnchor;
 import com.tugalsan.api.sql.select.server.TS_SQLSelectStmtUtils;
 import com.tugalsan.api.sql.where.server.TS_SQLWhere;
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
+import com.tugalsan.api.union.client.TGS_UnionExcuseVoid;
 
 public class TS_SQLExistsExecutor {
 
     public TS_SQLExistsExecutor(TS_SQLConnAnchor anchor, CharSequence tableName) {
         this.anchor = anchor;
-        this.tableName = tableName; 
+        this.tableName = tableName;
     }
     final public TS_SQLConnAnchor anchor;
     final public CharSequence tableName;
@@ -25,18 +26,36 @@ public class TS_SQLExistsExecutor {
         return sb.toString();
     }
 
-    public boolean run() {
-        TGS_Tuple1<Long> pack = new TGS_Tuple1(-1);
-        TS_SQLSelectStmtUtils.select(anchor, toString(), fillStmt -> {
+    public TGS_UnionExcuse<Boolean> run() {
+        var wrap = new Object() {
+            TGS_UnionExcuse<Integer> u_fill;
+            TGS_UnionExcuse<Boolean> u_empty;
+            TGS_UnionExcuse<Long> u_lng;
+            TGS_UnionExcuseVoid u_select;
+        };
+        wrap.u_select = TS_SQLSelectStmtUtils.select(anchor, toString(), fillStmt -> {
             if (where != null) {
-                where.fill(fillStmt, 0);
+                wrap.u_fill = where.fill(fillStmt, 0);
             }
         }, rs -> {
-            if (rs.row.isEmpty()) {
+            wrap.u_empty = rs.row.isEmpty();
+            if (wrap.u_empty.isExcuse() || wrap.u_empty.value()) {
                 return;
             }
-            pack.value0 = rs.lng.get(0, 0);
+            wrap.u_lng = rs.lng.get(0, 0);
         });
-        return pack.value0 == -1 ? null : pack.value0 != 0;
+        if (wrap.u_fill != null && wrap.u_fill.isExcuse()) {
+            return wrap.u_fill.toExcuse();
+        }
+        if (wrap.u_empty != null && wrap.u_empty.isExcuse()) {
+            return wrap.u_empty.toExcuse();
+        }
+        if (wrap.u_lng != null && wrap.u_lng.isExcuse()) {
+            return wrap.u_lng.toExcuse();
+        }
+        if (wrap.u_select != null && wrap.u_select.isExcuse()) {
+            return wrap.u_select.toExcuse();
+        }
+        return TGS_UnionExcuse.of(wrap.u_lng.value() != 0L);
     }
 }
